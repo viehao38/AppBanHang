221c63ccda8466aadccfb46dce84571ad236ccec
QuangDuy47 <nguyenduy4157@gmail.com>
2023-10-15 19:37:38 +0700

    Thiet ke don hang

--- app_banhang/lib/main.dart
+++ app_banhang/lib/main.dart
@@ -15,10 +15,50 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Nhom 1444444"),
-        ),
+      home: const MyHomePage(title: 'Nhóm 14'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(children: [
+          Row(children: const [Expanded(child: Divider(height: 10,thickness: 3,color: Colors.black,))],),
+          const SizedBox(height: 10,),
+          Row(mainAxisAlignment:MainAxisAlignment.center,
+              children: const [Icon(Icons.find_in_page),Text("Đơn hàng của tôi"),],),
+          const SizedBox(height: 10,),
+          Row(children: const [Expanded(child: Divider(height: 5,thickness: 3,color: Colors.black,))],),
+          Container(
+            color:const Color.fromARGB(255, 215, 208, 208),
+            child:Row(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: [
+                        Column(children: const [Icon(Icons.computer_outlined),Text("Đã đặt"),],),
+                        const SizedBox(width: 50),
+                        Column(children: const [Icon(Icons.done,color: Colors.green,),Text("Thành công")]),
+                        const SizedBox(width: 50),
+                        Column(children: const [Icon(Icons.highlight_remove,color: Colors.red,),Text("Hủy")],)],)),
+          Row(children: const[Text("  ")],),
+          Row(mainAxisAlignment:MainAxisAlignment.center,
+              children: const [Text("Xem thêm"),Icon(Icons.check_box)],),
+          Row(children: const [Expanded(child: Divider(height: 5,thickness: 3,color: Colors.black,))],)
+          ]),
       ),
     );
   }

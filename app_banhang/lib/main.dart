225740d5946ffbeddc70535c60409e3d299484cb
viehao38 <nguyenhaovkag@gmail.com>
2024-03-06 23:40:07 +0700

    to

--- app_banhang/lib/main.dart
+++ app_banhang/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
           fontFamily: "VLAMPLESOFT-MEDIUM",
           primarySwatch: Colors.blue,
